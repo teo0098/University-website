5a5c5cf8a98d6b16ed107132aba7b81e9881a290
Teodor <teodor.tkaczyk0407@gmail.com>
2019-12-27 15:39:29 +0100

    Fix server bug

--- backend/server.ts
+++ backend/server.ts
@@ -13,7 +13,19 @@ let errorMessage: string | null = null;
 let majors: Array<string> = [];
 
 connection.on('error', (err) => {
-    console.log(err.code);
+    connection.connect((error) => {
+        if (error) errorMessage = 'Unable to connect to the database, please try again later';
+        else {
+            connection.query('SELECT major_name FROM majors', (error, result) => {
+                if (error) errorMessage = 'Sorry for any issues, we couldn\'t load our university majors for you, please try again later';
+                else {
+                    result.forEach((value) => {
+                        majors.push(value.major_name);
+                    });
+                }
+            });
+        }
+    });
 });
 
 connection.connect((error) => {
